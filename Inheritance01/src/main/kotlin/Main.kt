a5de1a8055a94c7b31bf62df957bae23095a5ccd
vijay-chaudhari17 <vijaychaudhari0017@gmail.com>
2023-09-14 19:59:15 +0530

    Final Commit

--- Inheritance01/src/main/kotlin/Main.kt
+++ Inheritance01/src/main/kotlin/Main.kt
@@ -1,7 +1,44 @@
+/*
+    * Inheritance
+    * Base Class/ Parent Class/ Super Class -> The class from which you are inheriting
+    *
+    * */
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val car = Car("Ford", "Black", 4)
+    val plane = Plane("Boeing", "White & Blue", 4, 8)
+    car.stop()
+    car.move()
+    println()
+    plane.move()
+    plane.stop()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+}
+
+open class Vehicle(val name: String, val color: String){
+    open fun move(){
+        println("$name is moving")
+    }
+    open fun stop(){
+        println("$name has stopped")
+    }
+}
+
+class Car(name: String, color: String, val doors: Int): Vehicle(name, color){
+    override fun move(){
+        speeding()
+        super.move()
+    }
+    fun speeding(){
+        println("I am speed")
+    }
+}
+
+class Plane(name: String, color: String, val engines: Int, val doors: Int): Vehicle(name, color){
+    override fun move(){
+        flying()
+    }
+    fun flying(){
+        println("The plane is flying")
+    }
+
 }
