a92a859d85fbdb7ba967dd21f0bdae36216e477a
vijay-chaudhari17 <vijaychaudhari0017@gmail.com>
2023-09-14 20:21:06 +0530

    Updating Main.kt

--- A42_Inheritance02/src/main/kotlin/Main.kt
+++ A42_Inheritance02/src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val view = View()
+    val button = Button("Button", "Center")
+    val roundButton = RoundButton("RoundButton", "Left", 180)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    view.draw()
+    button.draw()
+    roundButton.draw()
 }
+
+open class View(){
+    open fun draw(){
+        println("Drawing the view")
+    }
+}
+open class Button(val text: String, val orientation: String): View(){
+    override fun draw(){
+        //here is the code for creating a button
+        println("Drawing the button")
+        super.draw()
+    }
+}
+class RoundButton(text: String, orientation: String, val corners: Int): Button(text, orientation){
+    override fun draw(){
+        println("Drawing the round button")
+        super.draw()
+    }
+}
+
